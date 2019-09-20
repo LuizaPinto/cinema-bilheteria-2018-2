165e795051126707aa824251425de70af7f7ddd0
LuizaPinto <54412214+LuizaPinto@users.noreply.github.com>
2019-09-20 11:50:47 -0300

    Update generosetor final.R

--- generosetor final.R
+++ generosetor final.R
@@ -1,36 +1,23 @@
 ### 
-
-y <- table(BD_Bilheteria2_20_202018$Genre)
-class(y)
-pie(y)
-title(2018)
-
-##
+###China Setor China
+require(tidyverse)
 genero_filme <- function(data, p="Brazil"){
   Base1 = data %>% 
-    dplyr::select(Genre)
+    filter(Country == p) %>% 
+    select(Genre)
   return(Base1)
 }
-genero_filme(BD_bilheteria2, p = "Brazil")
 
-# Dados para o gráfico - Brazil 
+SetorBrazil = genero_filme(BD_Bilheteria2_20_202018, p = "Brazil")
+W = table(SetorBrazil)
 
-freq <- round(sort(100*y/sum(y)),2)
+freq <- round(sort(100*W/sum(W)),2)
 freq
-
-generos <- as.vector(freq)
-names(freq)
-pie(freq)
-
-## Tratando - Brazil
-
-freq1 <- c(sum(generos[1 : 4]), generos[5:11])
-names(freq1) <-  c("Outros", names(freq[5:11]))
-
+freq1 <- W
+names(freq1) <-  c( names(freq[1:11]))
 porc<-round(freq1*100/sum(freq1), 2)
 rotulos<-paste(names(freq1),"(",porc,"%)", sep="")
-pie(freq1, main="Genêros no Brasil 2018", labels=rotulos, cex=0.7, col=rainbow(8))
-
+pie(freq1, main="Genêros na Brazil 2018", labels=rotulos, cex=0.7, col=rainbow(8))
 
 
 
